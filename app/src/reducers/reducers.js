a788e38c1fafc1dd10ea6126bcda6b593cd2f445
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-06-25 10:51:56 +0100

    .

--- app/src/reducers/reducers.js
+++ app/src/reducers/reducers.js
@@ -1,6 +1,28 @@
+import quotesData from '../quotesData';
 import {
     ADD_QUOTE,
     DELETE_QUOTE,
     MARK_QUOTE_OF_THE_DAY,
     MARK_FAVOURITE
 } from '../actions/actionTypes';
+
+// quotesReducer
+export function quotesReducer(sliceOfState = quotesData, action) {
+    switch (action.type) {
+        case DELETE_QUOTE:
+            return sliceOfState.filter(quote => quote.id !== action.payload);
+        case ADD_QUOTE:
+            // Do stuff
+        break;
+        case MARK_FAVOURITE:
+            // Do stuff
+        break;
+        default:
+        // Do stuff
+    }
+}
+
+// quoteOfTheDayReducer
+export function quoteOfTheDayReducer(sliceOfState = null, action) {
+
+}
